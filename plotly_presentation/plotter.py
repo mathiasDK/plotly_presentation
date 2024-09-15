9af4897d53718761584319fe13b5a50cbfdd0431
mathiasDK <44413262+mathiasDK@users.noreply.github.com>
2024-09-15 07:50:02 +0200

    Initial setup

---
+++ plotly_presentation/plotter.py
@@ -0,0 +1,23 @@
+import plotly.graph_objects as go
+import plotly.express as px
+
+class Plotter:
+    def __init__(self)->None:
+        self.figure = go.Figure()
+
+    def _initialize_figure(self)->None:
+        pass
+
+    def express(self, type:str, **args, **kwargs)->go.Figure:
+        self.figure = getattr(px, type)(**args, **kwargs)
+        return self.figure
+
+    def add_trace(self, func)->go.Figure:
+        self.figure.add_trace(func)
+        return self.figure
+
+    def show(self):
+        self.figure.show()
+
+    def save(self, path):
+        self.figure.write_image(path)
