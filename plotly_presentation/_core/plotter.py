aaad50d479164e38f29ff992df9a85fc81873ad9
Mathias Nørskov <bondnoerskov@hotmail.com>
2024-09-29 07:51:48 +0200

    Adding a few test

--- plotly_presentation/_core/plotter.py
+++ plotly_presentation/_core/plotter.py
@@ -3,17 +3,22 @@ import plotly.express as px
 
 class Plotter:
     def __init__(self)->None:
-        self._initialize_figure()
-
-    def _initialize_figure(self)->None:
         self.figure = go.Figure()
 
-    def express(self, type:str, **args, **kwargs)->go.Figure:
-        self.figure = getattr(px, type)(**args, **kwargs)
+    def _initialize_figure(self)->None:
+        pass
+
+    def _apply_settings(self)->None:
+        pass
+
+    def express(self, type:str, **kwargs)->go.Figure:
+        self.figure = getattr(px, type)(**kwargs)
+        self._apply_settings()
         return self.figure
 
     def add_trace(self, func)->go.Figure:
         self.figure.add_trace(func)
+        self._apply_settings()
         return self.figure
 
     def show(self):
