3c4f6d2b74197162e94a376232e220c072ddb7f5
Mathias Nørskov <mat@lunar.app>
2024-10-17 06:54:58 +0200

    cleanup

--- plotly_presentation/_core/options.py
+++ plotly_presentation/_core/options.py
@@ -1,23 +1,8 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (c) 2017-2018 Spotify AB
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 from collections import OrderedDict
 import os
 from pathlib import Path
 import yaml
-from plotly_presentation._core.utils.root_searcher import find_git_root, get_file_path
+from plotly_presentation._core.utils.root_searcher import get_file_path
 
 
 class Options:
--- plotly_presentation/_core/utils/root_searcher.py
+++ plotly_presentation/_core/utils/root_searcher.py
@@ -1,20 +1,4 @@
-import os
 import importlib.resources
-
-
-def find_git_root(path=os.getcwd()):
-    venv = os.getenv("VIRTUAL_ENV")
-    if venv:
-        return venv
-    current_path = path
-    while current_path != os.path.dirname(
-        current_path
-    ):  # Stop when we reach the filesystem root
-        if os.path.isdir(os.path.join(current_path, ".git")):
-            return current_path
-        current_path = os.path.dirname(current_path)
-    return None
-
 
 def get_file_path(file_name):
     base_path = importlib.resources.files("plotly_presentation")
