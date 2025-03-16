26c57993ddd1831e42e5c394a83301da8e7bdc32
mathiasDK <44413262+mathiasDK@users.noreply.github.com>
2025-03-16 16:33:18 +0100

    Add more testing (#18)
    Co-authored-by: Mathias Nørskov <bondnoerskov@hotmail.com>
    Co-authored-by: Mathias Nørskov <mat@lunar.app>

--- tests/_core/test_callouts.py
+++ tests/_core/test_callouts.py
@@ -115,6 +115,98 @@ class CalloutTest(unittest.TestCase):
         trace_count = len(p.figure.data)
         self.assertEqual(trace_count, 5)
 
+    def test_end_line_marker_text_format_value(self):
+        p = Plotter()
+        p.express(
+            type="line",
+            x=["cat1", "cat1", "cat2", "cat2", "cat3", "cat3", "cat4", "cat4"],
+            y=[1, 3, 2, 3, 3, 2, 4, 4],
+            color=["a", "b", "a", "b", "a", "b", "a", "b"],
+        )
+        p.callout.add_line_end_marker(
+            traces=["a"],
+            text_type="value",
+            text_format=".2f",
+        )
+        expected_text = "4.00"
+        for t in p.figure.select_traces(selector={"name": "a_marker"}):
+            actual_text = t.text[0]
+            self.assertEqual(expected_text, actual_text)
+
+    def test_end_line_marker_text_format_category(self):
+        p = Plotter()
+        p.express(
+            type="line",
+            x=["cat1", "cat1", "cat2", "cat2", "cat3", "cat3", "cat4", "cat4"],
+            y=[1, 3, 2, 3, 3, 2, 4, 4],
+            color=["a", "b", "a", "b", "a", "b", "a", "b"],
+        )
+        p.callout.add_line_end_marker(
+            traces=["a"],
+            text_type="category",
+        )
+        expected_text = "a"
+        for t in p.figure.select_traces(selector={"name": "a_marker"}):
+            actual_text = t.text[0]
+            self.assertEqual(expected_text, actual_text)
+
+    def test_end_line_marker_text_format_value_category(self):
+        p = Plotter()
+        p.express(
+            type="line",
+            x=["cat1", "cat1", "cat2", "cat2", "cat3", "cat3", "cat4", "cat4"],
+            y=[1, 3, 2, 3, 3, 2, 4, 4],
+            color=["a", "b", "a", "b", "a", "b", "a", "b"],
+        )
+        p.callout.add_line_end_marker(
+            traces=["a"],
+            text_type="value+category",
+            text_format=".0f",
+        )
+        expected_text = "4<br>a"
+        for t in p.figure.select_traces(selector={"name": "a_marker"}):
+            print(t)
+            actual_text = t.text[0]
+            self.assertEqual(expected_text, actual_text)
+
+    def test_end_line_marker_text_format_category_value(self):
+        p = Plotter()
+        p.express(
+            type="line",
+            x=["cat1", "cat1", "cat2", "cat2", "cat3", "cat3", "cat4", "cat4"],
+            y=[1, 3, 2, 3, 3, 2, 4, 4],
+            color=["a", "b", "a", "b", "a", "b", "a", "b"],
+        )
+        p.callout.add_line_end_marker(
+            traces=["a"],
+            text_type="category+value",
+            text_format=".0f",
+        )
+        expected_text = "a<br>4"
+        for t in p.figure.select_traces(selector={"name": "a_marker"}):
+            print(t)
+            actual_text = t.text[0]
+            self.assertEqual(expected_text, actual_text)
+
+    def test_end_line_marker_text_format_unknown_text_type(self):
+        p = Plotter()
+        p.express(
+            type="line",
+            x=["cat1", "cat1", "cat2", "cat2", "cat3", "cat3", "cat4", "cat4"],
+            y=[1, 3, 2, 3, 3, 2, 4, 4],
+            color=["a", "b", "a", "b", "a", "b", "a", "b"],
+        )
+
+        with self.assertRaises(AttributeError) as context:
+            p.callout.add_line_end_marker(
+                traces=["a"],
+                text_type="unknown",
+            )
+            self.assertEqual(
+                str(context.exception),
+                "The text type must be on of the following: {_VALID_TEXT_TYPES}",
+            )
+
     def test_get_center_point_categorical_x(self):
         p = Plotter()
         p.express(
@@ -173,3 +265,130 @@ class CalloutTest(unittest.TestCase):
         print(actual_center)
         expected_center = pd.to_datetime("2018-01-15 00:00:00")
         self.assertEqual(actual_center, expected_center)
+
+    def test_valid_circle_shapes(self):
+        p = Plotter()
+        try:
+            p.callout.add_circle_highlight(x=0, y=0, shape_form="round")
+        except ValueError:
+            self.fail("'round' is a valid input shape.")
+
+        try:
+            p.callout.add_circle_highlight(x=0, y=0, shape_form="oval")
+        except ValueError:
+            self.fail("'oval' is a valid input shape.")
+
+    def test_default_circle_highlight_size(self):
+        p = Plotter()
+        p.callout.add_circle_highlight(x=0, y=0)
+        expected_radius = p.callout._DEFAULT_CIRCLE_SIZE.get("circular_radius")
+        actual_x_radius = p.figure.layout.shapes[0]["x1"]
+        actual_y_radius = p.figure.layout.shapes[0]["y1"]
+        self.assertEqual(expected_radius, actual_x_radius)
+        self.assertEqual(expected_radius, actual_y_radius)
+
+    def test_square_growth_line(self):
+        p = Plotter()
+        p.callout.add_square_growth_line(x0=0, y0=0, x1=1, y1=1, y_top=1.5)
+
+        expected_vertical_1 = {
+            "x0": 0,
+            "y0": 0,
+            "x1": 0,
+            "y1": 1.5,
+        }
+        expected_horisontal = {
+            "x0": 0,
+            "y0": 1.5,
+            "x1": 1,
+            "y1": 1.5,
+        }
+        expected_vertical_2 = {
+            "x0": 1,
+            "y0": 1.5,
+            "x1": 1,
+            "y1": 1,
+        }
+        expected_shapes = [
+            expected_vertical_1,
+            expected_horisontal,
+            expected_vertical_2,
+        ]
+        for expected_shape, s in zip(expected_shapes, p.figure.layout.shapes):
+            actual_shape = {
+                "x0": s["x0"],
+                "y0": s["y0"],
+                "x1": s["x1"],
+                "y1": s["y1"],
+            }
+            self.assertEqual(actual_shape, expected_shape)
+
+    def test_dash_growth_lines_no_text(self):
+        p = Plotter()
+        p.callout.add_dash_growth_lines(x0=0, y0=0, x1=1, y1=1, x_end=1.5)
+
+        expected_dash_1 = {
+            "x0": 0,
+            "y0": 0,
+            "x1": 1.5,
+            "y1": 0,
+        }
+        expected_dash_2 = {
+            "x0": 1,
+            "y0": 1,
+            "x1": 1.5,
+            "y1": 1,
+        }
+        expected_shapes = [
+            expected_dash_1,
+            expected_dash_2,
+        ]
+        for expected_shape, s in zip(expected_shapes, p.figure.layout.shapes):
+            actual_shape = {
+                "x0": s["x0"],
+                "y0": s["y0"],
+                "x1": s["x1"],
+                "y1": s["y1"],
+            }
+            self.assertEqual(actual_shape, expected_shape)
+
+        actual_shapes = len(p.figure.layout.shapes)
+        self.assertEqual(actual_shapes, 2)
+
+        actual_annotations = len(p.figure.layout.annotations)
+        self.assertEqual(actual_annotations, 1)
+
+    def test_dash_growth_lines_no_text(self):
+        p = Plotter()
+        p.callout.add_dash_growth_lines(x0=0, y0=0, x1=1, y1=1, x_end=1.5, text="s")
+
+        expected_dash_1 = {
+            "x0": 0,
+            "y0": 0,
+            "x1": 1.5,
+            "y1": 0,
+        }
+        expected_dash_2 = {
+            "x0": 1,
+            "y0": 1,
+            "x1": 1.5,
+            "y1": 1,
+        }
+        expected_shapes = [
+            expected_dash_1,
+            expected_dash_2,
+        ]
+        for expected_shape, s in zip(expected_shapes, p.figure.layout.shapes):
+            actual_shape = {
+                "x0": s["x0"],
+                "y0": s["y0"],
+                "x1": s["x1"],
+                "y1": s["y1"],
+            }
+            self.assertEqual(actual_shape, expected_shape)
+
+        actual_shapes = len(p.figure.layout.shapes)
+        self.assertEqual(actual_shapes, 3)
+
+        actual_annotations = len(p.figure.layout.annotations)
+        self.assertEqual(actual_annotations, 2)
--- tests/_core/test_style.py
+++ tests/_core/test_style.py
@@ -138,6 +138,37 @@ class StyleTest(unittest.TestCase):
         actual_colors = {d.name: d.line.color for d in p.figure.data}
         self.assertEqual(new_colors, actual_colors)
 
+    def test_set_color_palette_invalid_palette_type(self):
+        p = Plotter()
+        p.express(type="bar", data_frame=self.df, x="date", y=["GOOG", "AAPL", "FB"])
+        _VALID_PALETTES = [
+            "sequential",
+            "diverging",
+            "sequential_negative",
+            "diverging_negative",
+        ]
+
+        with self.assertRaises(ValueError) as context:
+            p.style.set_color_palette(
+                palette_type="some palette that does not exist", palette_name="reds"
+            )
+            # Optionally, check the exception message
+            self.assertEqual(
+                str(context.exception),
+                f"Invalid palette type. Must be one of {_VALID_PALETTES}",
+            )
+
+    def test_set_color_palette_invalid_input(self):
+        p = Plotter()
+        p.express(type="bar", data_frame=self.df, x="date", y=["GOOG", "AAPL", "FB"])
+
+        with self.assertRaises(AttributeError) as context:
+            p.style.set_color_palette(palette_type=None, color_dict=None)
+            self.assertEqual(
+                str(context.exception),
+                "Either palette_type or color_dict must be provided",
+            )
+
     def test_waterfall_style(self):
         figure = go.Figure(
             go.Waterfall(
@@ -180,3 +211,47 @@ class StyleTest(unittest.TestCase):
             "connectors": "black",
         }
         self.assertEqual(actual_colors, expected_colors)
+
+    def test_slide_layout_size(self):
+        p = Plotter()
+        p.express(type="bar", data_frame=self.df, x="date", y=["GOOG", "AAPL", "FB"])
+
+        p.style._set_width_and_height("slide_25%")
+        self.assertEqual(960.0 / 2, p.figure.layout.width)
+        self.assertEqual(540.0 / 2, p.figure.layout.height)
+
+        p.style._set_width_and_height("slide_50%")
+        self.assertEqual(960.0 / 2, p.figure.layout.width)
+        self.assertEqual(540.0, p.figure.layout.height)
+
+        p.style._set_width_and_height("slide_75%")
+        self.assertEqual(960.0 * 0.75 * 0.8, p.figure.layout.width)
+        self.assertEqual(540.0 * 0.8, p.figure.layout.height)
+
+        p.style._set_width_and_height("slide_100%")
+        self.assertEqual(960.0, p.figure.layout.width)
+        self.assertEqual(540.0, p.figure.layout.height)
+
+        p.style._set_width_and_height("slide_wide")
+        self.assertEqual(960.0, p.figure.layout.width)
+        self.assertEqual(540 * 0.75, p.figure.layout.height)
+
+    def test_unknown_sequential_palette(self):
+        p = Plotter()
+        p.express(type="bar", data_frame=self.df, x="date", y=["GOOG", "AAPL", "FB"])
+        with self.assertRaises(TypeError) as context:
+            p.style.set_color_palette(palette_type="sequential", palette_name="unknown")
+            self.assertEqual(
+                str(context.exception),
+                "Invalid palette name. Must be one of {palette_names}",
+            )
+
+    def test_unknown_divergin_palette(self):
+        p = Plotter()
+        p.express(type="bar", data_frame=self.df, x="date", y=["GOOG", "AAPL", "FB"])
+        with self.assertRaises(TypeError) as context:
+            p.style.set_color_palette(palette_type="diverging", palette_name="unknown")
+            self.assertEqual(
+                str(context.exception),
+                "Invalid palette name. Must be one of {palette_names}",
+            )
